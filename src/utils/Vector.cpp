4b6542141b8cb3b0e2c05154d31d37481365c92c
tywhite14 <twhite2037@gmail.com>
2024-09-20 17:45:13 -0700

    add Vec3d

--- src/utils/Vector.cpp
+++ src/utils/Vector.cpp
@@ -4,16 +4,16 @@
 #include <iomanip>
 #include <stdlib.h>
 
-Vec2d::Vec2d() : x(0.0), y(0.0) { }
+// ========================= Vect2d ========================= //
 Vec2d::Vec2d(const double s) : x(s), y(s) { }
 Vec2d::Vec2d(const double xi, const double yi) : x(xi), y(yi) {}
 
 Vec2d Vec2d::random() const
 {
 	double x, y;
-	x = (double) rand() / RAND_MAX;
-	y = (double) rand() / RAND_MAX;
-	return (x, y);
+	x = (double)rand() / RAND_MAX;
+	y = (double)rand() / RAND_MAX;
+	return Vec2d(x, y);
 }
 
 Vec2d Vec2d::randi(const int max, bool pm) const
@@ -23,12 +23,12 @@ Vec2d Vec2d::randi(const int max, bool pm) const
 	if (pm) {
 		x = rand() % (2 * (max + 1)) - max;
 		y = rand() % (2 * (max + 1)) - max;
-		return (x, y);
+		return Vec2d(x, y);
 	}
 
 	x = rand() % (max + 1);
 	y = rand() % (max + 1);
-	return (x, y);
+	return Vec2d(x, y);
 }
 
 Vec2d Vec2d::operator=(const std::initializer_list<double>& list)
@@ -36,14 +36,63 @@ Vec2d Vec2d::operator=(const std::initializer_list<double>& list)
 	if (list.size() != 2) {
 		FATAL("Initializer list is not the same size as matrix.", 9);
 	}
-	
+
 	x = *list.begin();
 	y = *list.end();
-	return (x, y);
+	return Vec2d(x, y);
 }
 
 std::ostream& operator<<(std::ostream& os, const Vec2d& v)
 {
 	os << v.x << ", " << v.y;
+	return os;
+}
+
+
+// ========================= Vect3d ========================= //
+Vec3d::Vec3d(const double s) : x(s), y(s), z(s) { }
+Vec3d::Vec3d(const double xi, const double yi, const double zi) : x(xi), y(yi), z(zi) {}
+
+Vec3d Vec3d::random() const
+{
+	double x, y, z;
+	x = (double) rand() / RAND_MAX;
+	y = (double) rand() / RAND_MAX;
+	z = (double) rand() / RAND_MAX;
+	return Vec3d(x, y, z);
+}
+
+Vec3d Vec3d::randi(const int max, bool pm) const
+{
+	double x, y, z;
+
+	if (pm) {
+		x = rand() % (2 * (max + 1)) - max;
+		y = rand() % (2 * (max + 1)) - max;
+		z = rand() % (2 * (max + 1)) - max;
+		return Vec3d(x, y, z);
+	}
+
+	x = rand() % (max + 1);
+	y = rand() % (max + 1);
+	z = rand() % (max + 1);
+	return Vec3d(x, y, z);
+}
+
+Vec3d Vec3d::operator=(const std::initializer_list<double>& list)
+{
+	if (list.size() != 3) {
+		FATAL("Initializer list is not the same size as matrix.", 9);
+	}
+
+	x = *list.begin();
+	y = *(list.begin() + 1);
+	y = *list.end();
+	return Vec3d(x, y, z);
+}
+
+std::ostream& operator<<(std::ostream& os, const Vec3d& v)
+{
+	os << v.x << ", " << v.y << ", " << v.z;
 	return os;
 }
--- src/utils/Vector.h
+++ src/utils/Vector.h
@@ -7,18 +7,17 @@
 class Vec2d
 {
 public:
-	Vec2d();
-	Vec2d(const double s);
+	Vec2d(const double s = 0.0);
 	Vec2d(const double xi, const double yi);
 
 	// members
-	inline double magnitude() const { return std::sqrt(x*x + y*y); }
-	inline Vec2d zeros() const { return Vec2d(0.0); };
-	inline Vec2d ones()  const { return Vec2d(1.0); };
+	inline double magnitude() const { return std::sqrt(x * x + y * y); }
 	Vec2d random() const;
 	Vec2d randi(const int max, bool pm = true) const;
-	inline Vec2d rotate(const double angle) const { return Vec2d(std::cos(angle) * x - std::sin(angle) * y, std::sin(angle) * x + std::cos(angle) * y); }
 	inline Vec2d rot90() const { return Vec2d(-y, x); }
+	inline Vec2d rotate(const double angle) const { 
+		return Vec2d(std::cos(angle) * x - std::sin(angle) * y, std::sin(angle) * x + std::cos(angle) * y);
+	}
 
 	// operator overloading
 	inline Vec2d operator-() const { return Vec2d(-x, -y); }
@@ -35,6 +34,31 @@ public:
 private:
 };
 
-inline Vec2d operator*(const double s, const Vec2d& v) {
-	return Vec2d(s * v.x, s * v.y);
-}
+class Vec3d
+{
+public:
+	Vec3d(const double s = 0);
+	Vec3d(const double xi, const double yi, const double zi);
+
+	// members
+	inline double magnitude() const { return std::sqrt(x*x + y*y + z*z); }
+	Vec3d random() const;
+	Vec3d randi(const int max, bool pm = true) const;
+
+	// operator overloading
+	inline Vec3d operator-() const { return Vec3d(-x, -y, -z); }
+	inline Vec3d operator+(const Vec3d& b) const { return Vec3d(x + b.x, y + b.y, z + b.z); }
+	inline Vec3d operator-(const Vec3d& b) const { return Vec3d(x - b.x, y - b.y, z - b.z); }
+	inline Vec3d operator%(const Vec3d& b) const { return Vec3d(x * b.x, y * b.y, z * b.z); }
+	inline Vec3d operator*(const double s) const { return Vec3d(x * s, y * s, z * s); }
+	inline bool operator==(const Vec3d& b) const { return (x == b.x && y == b.y && z == b.z); }
+	Vec3d operator=(const std::initializer_list<double>& list);
+	friend std::ostream& operator<<(std::ostream& os, const Vec3d& v);
+
+	double x, y, z;
+
+private:
+};
+
+inline Vec2d operator*(const double s, const Vec2d& v) { return v * s; }
+inline Vec3d operator*(const double s, const Vec3d& v) { return v * s; }
